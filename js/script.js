2d0e67a89b669a4c1a4872158424d5f279054bec
Raimundo Pérez <raimundoprez@gmail.com>
2025-11-04 19:19:20 +0100

    Exercise finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
  // Aquí tu código
+const contentList = document.getElementById("lista");
+const contentButton = document.getElementById("agregar");
+
+contentButton.addEventListener("click", () => {
+    let elementType = prompt("Introduce un tipo de elemento para insertar.");
+    if (!elementType) elementType = "";
+
+    const elementCreated = elementType ? document.createElement(elementType) : null;
+    
+    if (!elementCreated || elementCreated instanceof HTMLUnknownElement) {
+        if (elementCreated) elementCreated.remove();
+        alert(`El tipo de elemento '${elementType}' no es un tipo de HTML válido y no se ha añadido a la lista.`);
+    }
+    else {
+        const container = document.createElement("div");
+        const banner = document.createElement("p");
+
+        banner.textContent = `${elementType} ==>`;
+
+        container.appendChild(banner);
+        container.appendChild(elementCreated);
+        contentList.appendChild(container);
+    }
+});
